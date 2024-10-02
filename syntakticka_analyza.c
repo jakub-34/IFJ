1270ebc1dd1698ed625df5073f0dc243d40b2ec3
Jakub <jakubhrdlicka34@gmail.com>
2024-10-02 16:47:30 +0200

    feat: add comments

--- syntakticka_analyza.c
+++ syntakticka_analyza.c
@@ -5,48 +5,71 @@
 #include "token.h"
 
 
+// <VARIABLE>
 int variable(token_t *token){
-    return(((strcmp(token->data, "var") == 0) || (strcmp(token->data, "const") == 0)) ? 0 : 1);
+    // <VARIABLE> -> var
+    if(strcmp(token->data, "var") == 0){
+        return 0;
+    }
+
+    // <VARIABLE> -> const
+    if(strcmp(token->data, "const") == 0){
+        return 0;
+    }
+
+    return 1;
 }
 
 
+// <TYPE>
 void type(token_t *token){
     // TODO
     return 0;
 }
 
 
+// <IN_PARAM_CONTINUATION>
 // !!! don't call get_token() after this function !!!
 token_t *in_param_continuation(token_t *token){
+    // <IN_PARAM_CONTINUATION> -> , <IN_PARAM>
     if(strcmp(token->data, ",") == 0){
         token = get_token();
-        in_param(token);
+        token = in_param(token);
         return token;
     }
+
+    // <IN_PARAM_CONTINUATION> -> ε
     return token;
 }
 
 
+// <IN_PARAM>
 // !!! don't call get_token() after this function !!!
 token_t *in_param(token_t *token){
+    // <IN_PARAM> -> ID <IN_PARAM_CONTINUATION>
     if(token->type == identifier_token){
         token = get_token();
         token = in_param_continuation(token);
         return token;
     }
 
+    // <IN_PARAM> -> <TERM> <IN_PARAM_CONTINUATION>
     if(term() == 0){
         token = get_token();
         token = in_param_continuation(token);
         return token;
     }
 
+    // <IN_PARAM> -> ε
     return token;
 }
 
 
+// <NEXT_VARIABLE_CONTINUATION>
 void next_variable_continuaton(token_t *token){
     // TODO expression
+
+    // <NEXT_VARIABLE_CONTINUATION> -> ID ( <IN_PARAM> )
     if(token->type == identifier_token){
         token = get_token();
         if(strcmp(token->data, "(") != 0){
@@ -67,7 +90,9 @@ void next_variable_continuaton(token_t *token){
 }
 
 
+// <VARIABLE_CONTINUATION>
 void variable_continuation(token_t *token){
+    // <VARIABLE_CONTINUATION> -> : <TYPE> = <NEXT_VARIABLE_CONTINUATION>
     if(strcmp(token->data, ":") == 0){
         token = get_token();
         type(token);
@@ -81,6 +106,7 @@ void variable_continuation(token_t *token){
         return;
     }
 
+    // <VARIABLE_CONTINUATION> -> = <NEXT_VARIABLE_CONTINUATION>
     if(strcmp(token->data, "=") == 0){
         token = get_token();
         next_variable_continuaton(token);
@@ -92,7 +118,9 @@ void variable_continuation(token_t *token){
 }
 
 
+// <CODE>
 void code(token_t *token){
+    // <CODE> -> <VARIABLE> ID <VARIABLE_CONTINUATION> ; <CODE>
     if(variable(token) == 0){
         token = get_token();
         if(token->type != identifier_token){
