b892900f847638c567de4704c97cd15c584ecc92
Kubikuli <Kubaluc@seznam.cz>
2024-11-10 16:40:56 +0100

    Added some comments and fixed segfault in printAST

--- ast.c
+++ ast.c
@@ -6,19 +6,22 @@
 
 
 // Creates and initializes AST
+// Returns pointer to the newly created AST
 AST *createAST() {
     AST *ast = malloc(sizeof(AST));
     if (ast == NULL) {
         fprintf(stderr, "Error allocating memory for AST\n");
         exit(99);
     }
+    // Initializes to default values
     ast->active = NULL;
     ast->currentLine = NULL;
     ast->root = NULL;
     return ast;
 }
 
-// Creates new node and saves "token" as its value
+// Creates new node, saves "token" as its value
+// and moves ast->active to point to the new node
 void create_node(token_t *token, AST *ast){
     ASTNode *node = malloc(sizeof(ASTNode)); // allocating for node
     if (node == NULL) {
@@ -30,32 +33,31 @@ void create_node(token_t *token, AST *ast){
     node->newLine = NULL; // left child
     node->token = token;
 
-    // If theres no active node == tree is empty
+    // If theres no active node => tree is empty
     // Sets new node as root
     if (ast->active == NULL) {
         ast->root = node;
         ast->currentLine = node;
     }
-    // Makes current active node (last added to tree) point to new node
+    // Makes current active node point to new node
     else {
         ast->active->next = node;
     }
-
     // Moves the active pointer to new node
     ast->active = node;
 }
 
+// Moves ast->active to next node and returns value of ast->active
 ASTNode *next_node(AST *ast) {
     // If tree was uninitialized
     if (ast->active == NULL){
-        return ast->root;
+        ast->active = ast->root;
     }
-
-    // Moves the pointer
-    ast->active = ast->active->next;
-
-    // printf("Next node: %s\n", ast->active->token->data);
-
+    else {
+        // Moves the pointer
+        ast->active = ast->active->next;
+    }
+    
     return ast->active;
 }
 
@@ -63,8 +65,7 @@ ASTNode *next_node(AST *ast) {
 void printAST(AST *ast){
     ast->active = ast->root;
 
-    while(ast->active != NULL){
-        // printf("%p\n", ast->active);
+    while(ast->active != NULL && ast->active->token->type != eof_token){
         printf("%s\n", ast->active->token->data);
         next_node(ast);
     }
